8ee56f0d14eb2d8dffa8041f522774952e9297a0
VinzJS <99986578+VinzJS@users.noreply.github.com>
2022-02-19 01:39:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
    <h1>Welcome to My website! I am Vince Jude!</h1> 
    <ul>
        <li><a href="index.php">HOME</a></li>
-       <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+       <li><a href="/LT2.html">LEARNING TASK 2</a></li>
    </ul>
    </body>
 </html>
