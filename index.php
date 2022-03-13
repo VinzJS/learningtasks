4041b4ad9bef9b4e833f5ec0001c937da2df5a7e
VinzJS <99986578+VinzJS@users.noreply.github.com>
2022-03-13 16:14:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,12 @@
           <p>Kendrick Lamar Duckworth is an American rapper, songwriter, and record producer.  
         He is widely regarded as one of the greatest rappers of all time.</p>
      </div>
-     <div class="col"><p>Top 2</p></div>
+     <div class="col"><p>Top 2 Tupac</p>
+     <img src="tupac.jpg" alt="2pac">
+          <p>Tupac Amaru Shakur, better known by his stage name 2Pac and later by his alias Makaveli, was an American rapper and actor. 
+               Considered one of the most influential rappers of all time, 
+               Shakur is among the best-selling music artists, having sold more than 75 million records worldwide</p>
+     </div>
      <div class="col"><p>Top 3</p></div>
      <div class="col"><p>Top 4</p></div>
      <div class="col"><p>Top 5</p></div>
