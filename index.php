0645608b95b641ecea682196c5ece474a3213b64
VinzJS <99986578+VinzJS@users.noreply.github.com>
2022-03-13 16:39:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,14 +15,44 @@
           <p>Tupac Amaru Shakur, better known by his stage name 2Pac. Shakur is among the best-selling music artists, having sold more than 75 million records worldwide 
                Considered one of the most influential rappers of all time.</p>
      </div>
-     <div class="col"><p>Top 3</p></div>
-     <div class="col"><p>Top 4</p></div>
-     <div class="col"><p>Top 5</p></div>
-     <div class="col"><p>Top 6</p></div>
-     <div class="col"><p>Top 7</p></div>
-     <div class="col"><p>Top 8</p></div>
-     <div class="col"><p>Top 9</p></div>
-     <div class="col"><p>Top 10</p></div>
+     <div class="col"><p>Top 3 Notorious B.I.G</p>
+          <img src="big.jpg" alt="big">
+          <p> Better known by his stage names the Notorious B.I.G., Biggie Smalls, or simply Biggie, was an American rapper and songwriter. 
+               Rooted in the New York rap scene and gangsta rap traditions, he is widely considered one of the greatest rappers of all time</p>
+     </div>
+     <div class="col"><p>Top 4 Dr. Dre</p>
+          <img src="dre.jpg" alt="drdre">
+          <p>Known professionally as Dr. Dre, is an American rapper, record producer, and entrepreneur. 
+             He is the founder and CEO of Aftermath Entertainment and Beats Electronics, and previously co-founded, co-owned, and was the president of Death Row Records</p>
+     </div>
+     <div class="col"><p>Top 5 Eminem</p>
+          <img src="mnm.jpg" alt="mnm">
+          <p>Known professionally as Eminem, is an American rapper, songwriter, and record producer. 
+             Eminem is among the best-selling music artists of all time, with estimated worldwide sales of over 220 million records.</p>
+     </div>
+     <div class="col"><p>Top 6 50 Cent</p>
+          <img src="50.jpg" alt="cent">
+          <p>50 Cent, is an American rapper, actor and entrepreneur. 
+             Known for his impact in the hip hop industry, he has been described as a "master of the nuanced art of lyrical brevity".</p>
+     </div>
+     <div class="col"><p>Top 7 Snoop Dogg</p>
+          <img src="snoop.jpg" alt="dogg">
+          <p>Known professionally as Snoop Dogg, is an American rapper, songwriter, media personality, actor, and entrepreneur. 
+             His fame dates to 1992 when he featured on Dr. Dre's debut solo single, "Deep Cover", and then on Dre's debut solo album, The Chronic.</p>
+     </div>
+     <div class="col"><p>Top 8 Ice Cube</p>
+          <img src="ice.jpg" alt="cube">
+          <p>Ice Cube, is an American rapper, actor, and filmmaker. His lyrics on N.W.A's 1988 album Straight Outta Compton contributed to gangsta rap's widespread</p>
+     </div>
+     <div class="col"><p>Top 9 J Cole</p>
+          <img src="cole.jpg" alt="cole">
+          <p>Jermaine Lamarr Cole is an American rapper, singer, songwriter, and record producer. Cole is regarded as one of the most influential rappers of his generation.</p>
+     </div>
+     <div class="col"><p>Top 10 Drake</p>
+          <img src="drizzy.jpg" alt="drake">
+          <p>Drake is an Canadian rapper, singer, and actor. Gaining recognition by starring as Jimmy Brooks in the CTV teen drama series Degrassi: The Next Generation, 
+               Drake pursued a career in music</p>
+     </div>
 </div> 
 </body>
 </html>
