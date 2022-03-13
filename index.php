fbea4c043b19335463e2a71b4cc99e414a168d93
VinzJS <99986578+VinzJS@users.noreply.github.com>
2022-03-13 15:11:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <b>My top 10 favorites</b>
   </head>
   <body>
-    <div>Top 1
-      Kendrick Lamar
+    <div>
+      Top 1 Kendrick Lamar
       <img src="kendrick.jpg" alt="Kendrick"></div>
       <p>Kendrick Lamar Duckworth is an American rapper, songwriter, and record producer. Since his mainstream debut in 2012 with Good Kid, M.A.A.D City, Lamar has been regarded as one of the most influential rappers of his generation. He is widely regarded as one of the greatest rappers of all time.</p>
   </body>
