685d8de8b96b7e1c59ba8eab352af95cffcb2a4d
VinzJS <99986578+VinzJS@users.noreply.github.com>
2022-04-25 17:11:08 +0800

    Update java.js

--- LT5/js/java.js
+++ LT5/js/java.js
@@ -1,6 +1,13 @@
 var Song = document.getElementById("Song");
 var icon = document.getElementById("icon");
+var Song2 = document.getElementById("Song2");
+var icon2 = document.getElementById("icon2");
+document.getElementById("song2").volume = 0.2;
+document.getElementById("Song").volume = 0.2;
 
 icon.onclick = function(){
 Song.play();
+}
+icon2.onclick = function(){
+song2.play();
 }
